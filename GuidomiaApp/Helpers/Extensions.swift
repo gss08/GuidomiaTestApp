57aa39b53d1e5770ee32af93d8bcf9b6fc227112
Gobinder Singh <sidhugobinder@gmail.com>
2021-04-26 04:11:00 -0400

    Displayed data from JSON in table cell

---
+++ GuidomiaApp/Helpers/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  GuidomiaApp
+//
+//  Created by GSS on 2021-04-26.
+//
+
+import Foundation
---
+++ GuidomiaApp/Resources/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  GuidomiaApp
+//
+//  Created by GSS on 2021-04-26.
+//
+
+import Foundation
